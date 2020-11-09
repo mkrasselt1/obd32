2fa280ddce8a72a148d97c565144c4aeb230c88f
Michael Krasselt <michael@michaelkrasselt.de>
2020-11-09 15:34:59 +0100

    Add: Simple MQTT Client

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
-// Copyright (c) Sandeep Mistry. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 #include <CAN.h> // the OBD2 library depends on the CAN library
 #include <OBD2.h>
+#include <SPI.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+	
+//Wifi Credentials
+const char* ssid = "AP";
+const char* password =  "PW";
+#define mqtt_server "SERVERNAME_OR_IP"
+#define mqtt_port 1883
+#define mqtt_user "USERNAME"
+#define mqtt_password "PASSWORD"
 
 // array of PID's to print values of
 const int PIDS[] = {
@@ -11,42 +20,44 @@ const int PIDS[] = {
   VEHICLE_SPEED,
   THROTTLE_POSITION,
 };
-
 const int NUM_PIDS = sizeof(PIDS) / sizeof(PIDS[0]);
+
 void printPID(int pid);
+WiFiClient espClient;
+PubSubClient mqtt(espClient);
+
 void setup() {
   Serial.begin(115200);
+  WiFi.begin(ssid, password);
   while (!Serial);
-
-  Serial.println(F("OBD2 Key Stats"));
-
+  //Start Wifi
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  } 
+  Serial.println("OK!");
+  //Start CAN
   while (true) {
-    Serial.print(F("Attempting to connect to OBD2 CAN bus ... "));
-
     if (!OBD2.begin()) {
-      Serial.println(F("failed!"));
-
-      delay(1000);
+      Serial.println(F("Can init failed!"));
     } else {
-      Serial.println(F("success"));
-      break;
+      Serial.println(F("Can init success")); break;
     }
   }
-
-  Serial.println();
+  //MQTT
+  mqtt.setServer(mqtt_server, mqtt_port);
+  if (mqtt.connect("odb32", mqtt_user, mqtt_password)) {
+    mqtt.publish("odb32/status","running");
+    mqtt.subscribe("odb32/settings");
+  }
 }
 
 void loop() {
-  // loop through all the PID's in the array
-  // 
-  for (int i = 0; i < NUM_PIDS; i++) {
-    int pid = PIDS[i];
-
-    printPID(pid);
-  }
+  for (int i = 0; i < NUM_PIDS; i++) { printPID(PIDS[i]); }
   Serial.println();
-
   delay(100);
+  mqtt.loop();
 }
 
 void printPID(int pid) {
@@ -60,7 +71,16 @@ void printPID(int pid) {
   if (isnan(pidValue)) {
     Serial.print("error");
   } else {
+    char topic[20] = "obd32";
+    char sub_topic[20];
+    char result[8];
+    dtostrf(pidValue, 6, 2, result);
+    OBD2.pidName(pid).toCharArray(sub_topic, 20);
+    strcat(topic, "/");
+    strcat(topic, sub_topic);
     // print value with units
+    mqtt.publish(topic, result);
+
     Serial.print(pidValue);
     Serial.print(F(" "));
     Serial.print(OBD2.pidUnits(pid));
